Fixed a bug: added transforms.Normalize((0.5,), (0.5,)) to prohibit 0 gradients in the first layer

code/cubic_src/benchmarks_networks.py
@@ -166,6 +166,19 @@ def dataset(network_to_use_):
         return CIFAR10
 
 
+def getBack(var_grad_fn):
+    print(var_grad_fn)
+    for n in var_grad_fn.next_functions:
+        if n[0]:
+            try:
+                tensor = getattr(n[0], 'variable')
+                print(n[0])
+                print('Tensor with grad found:', tensor)
+                print(' - gradient:', tensor.grad)
+                print()
+            except AttributeError as e:
+                getBack(n[0])
+
 def weights_init(m):
     if isinstance(m, nn.Linear):
         #nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
@@ -198,7 +211,21 @@ def train(args, model, device, train_loader, optimizer, epoch, test_loader, crit
 
         loss = criterion(output, y_onehot)
         #loss = criterion(output, target)
+        #for i, param in enumerate(optimizer.param_groups[0]['params']):
+        #    print(i)
+        #    print('each param', param.size(), param)
+        #    print('its mean', param.grad)
+
+        #print(getBack(loss.grad_fn))
         loss.backward()
+        #for param in optimizer.param_groups[0]['params']:
+        #    if (param.grad is None or \
+        #        (param.grad.sum() == 0)) \
+        #        and optimizer.defaults['problem'] == 'matrix_completion':
+        #        continue
+        #    print('each param', param.size(), param)
+        #    print('its mean', param.grad)
+        #exit(0)
         optimizer.step()
         if batch_idx * len(data) % args.log_interval == 0 and batch_idx != 0:
             print('batch idx', batch_idx)
@@ -406,7 +433,7 @@ def main():
              transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]),
         'LIN_REG_MNIST': transforms.Compose([
             transforms.ToTensor(),
-            # transforms.Normalize((0.5,), (0.5,))
+            transforms.Normalize((0.5,), (0.5,))
         ])
     }
 

code/cubic_src/config.py
@@ -8,6 +8,7 @@ from torchvision.datasets import MNIST, CIFAR10
 import pytorch_optmizers
 import torch.nn.functional as F
 from torch import nn, optim
+import autograd_hacks
 import numpy as np
 from resnet_cifar import resnet20_cifar
 
@@ -16,16 +17,40 @@ torch.set_printoptions(precision=10)
 
 
 class Flatten(nn.Module):
+
+    def __init__(self):
+        super(Flatten, self).__init__()
+
     def forward(self, x):
-        return x.view(x.size(0), -1)
+        shape = torch.prod(torch.tensor(x.shape[1:])).item()
+        return x.view(-1, shape)
 
 
-def LinearRegression():
+def LinearRegression_():
     model_ = nn.Sequential(
         Flatten(),
-        nn.Linear(784, 10)
+        nn.Linear(784, 10, bias=True),
     )
     return model_
+
+
+#LinearRegression_ = nn.Sequential()
+#LinearRegression_.add_module('flat', Flatten())
+#LinearRegression_.add_module('weights', nn.Linear(784, 10, bias=True))
+
+
+class LinearRegression(torch.nn.Module):
+    def __init__(self):
+        super(LinearRegression, self).__init__()
+        self.out = nn.Sequential(
+            Flatten(),
+            nn.Linear(784, 10)
+        )
+
+    def forward(self, x):
+        out = self.out(x)
+        return out
+
 
 # Source - https://github.com/pytorch/examples/blob/master/mnist/main.py
 
@@ -160,7 +185,7 @@ def to_img(x):
 
 
 network_to_use = 'LIN_REG_MNIST'  # AE_MNIST, CNN_MNIST, CONV_AE_MNIST, CNN_CIFAR, LIN_REG_MNIST
-activation = 'swish'  # swish, softplus, relu
+activation = 'softplus'  # swish, softplus, relu
 
 transforms_dict = {
         'CNN_MNIST': transforms.Compose([
@@ -173,7 +198,7 @@ transforms_dict = {
                        ]),
         'LIN_REG_MNIST': transforms.Compose([
                            transforms.ToTensor(),
-                           #transforms.Normalize((0.5,), (0.5,))
+                           transforms.Normalize((0.5,), (0.5,))
                        ]),
         'CONV_AE_MNIST': transforms.Compose([
                            transforms.ToTensor(),
@@ -235,6 +260,9 @@ start_model_path = None
 # "models/benchmarks_AE_MNIST_Adam_5.pt"
 
 model = models[network_to_use].to(dev)
+
+#autograd_hacks.add_hooks(model)
+
 torch.manual_seed(7)
 if start_model_path:
     model.load_state_dict(torch.load(start_model_path))

code/cubic_src/train.py
@@ -107,10 +107,24 @@ def update_params(dct, src):
     return dct
 
 
+def getBack(var_grad_fn):
+    print(var_grad_fn)
+    for n in var_grad_fn.next_functions:
+        if n[0]:
+            try:
+                tensor = getattr(n[0], 'variable')
+                print(n[0])
+                print('Tensor with grad found:', tensor)
+                print(' - gradient:', tensor.grad)
+                print()
+            except AttributeError as e:
+                getBack(n[0])
+
 def main():
     train_flag = True
     n_digits = 10
     y_onehot = torch.FloatTensor(int(optimizer.defaults['sample_size_gradient']), n_digits)
+    autograd_hacks.add_hooks(optimizer.model)
 
     for epoch in range(args.epochs):
         # Set modules in the the network to train mode
@@ -150,13 +164,56 @@ def main():
             #print('Train data size ', data.size())
             optimizer.zero_grad()
             #print('Memory used zero_grad: ', psutil.virtual_memory().used >> 20)
-            outputs = model(data)
+
+            outputs = optimizer.model(data)
 
             y_onehot.zero_()
             if 'LIN_REG' in optimizer.defaults['problem']:
                 y_onehot.scatter_(1, target.view(-1, 1), 1)
-            loss_fn(outputs, y_onehot if 'LIN_REG' in optimizer.defaults['problem'] else target)\
-                .backward(create_graph=True)
+
+            try:
+                autograd_hacks.clear_backprops(optimizer.model)
+            except:
+                pass
+            """
+            for param in optimizer.param_groups[0]['params']:
+                if (param.grad is None or \
+                    (param.grad.sum() == 0)) \
+                        and optimizer.defaults['problem'] == 'matrix_completion':
+                    continue
+                print(param.requires_grad)
+                print('nname', param.names)
+                print('each param', param.size(), param)
+                print('its mean', param.grad)
+
+            print(optimizer.model.training)
+            print(model.training)
+            names = [n for n, p in optimizer.model.named_parameters() if p.requires_grad]
+            print(names)
+            names = [n for n, p in model.named_parameters() if p.requires_grad]
+            print(names)
+            #exit(0)
+            """
+            loss = loss_fn(outputs, y_onehot if 'LIN_REG' in optimizer.defaults['problem'] else target)
+            #print(getBack(loss.grad_fn))
+            #grads = torch.autograd.grad(loss
+            #                            , inputs=data, retain_graph=True, create_graph=True)
+            #print(grads)
+            #exit(0)
+            loss.backward(create_graph=True)
+            autograd_hacks.compute_grad1(optimizer.model)
+            #print(getBack(loss.grad_fn))
+            """
+            for param in optimizer.param_groups[0]['params']:
+                if (param.grad is None or \
+                    (param.grad.sum() == 0)) \
+                        and optimizer.defaults['problem'] == 'matrix_completion':
+                    continue
+
+                print('each param', param.size(), param)
+                print('after its mean', param.grad)
+            exit(0)
+            """
             #loss_fn(outputs, target).backward(create_graph=True)
             #print('Memory used loss: ', psutil.virtual_memory().used >> 20)
             optimizer.defaults['train_data'] = data

code/cubic_src/utils.py
@@ -11,6 +11,9 @@ from scipy.sparse.linalg import minres
 from torch.autograd import Variable
 from torch.optim.optimizer import Optimizer
 import matplotlib
+
+import autograd_hacks
+
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import os, datetime
@@ -125,6 +128,7 @@ class SRCutils(Optimizer):
         self.delta_m_norm = None
 
         self.first_hv = True
+        self.hv_temp = None
 
         # Momentum, experimental
         self.b_1 = 0.9
@@ -164,8 +168,8 @@ class SRCutils(Optimizer):
                              AccGD=opt.get('AccGD', False),
                              innerAdam=opt.get('innerAdam', False),
                              verbose=opt.get('verbose', False),
-                             n_iter=opt.get('n_iter', 4),
-                             block_size=opt.get('block_size', None),
+                             n_iter=opt.get('n_iter', 1),
+                             block_size=opt.get('block_size', 100),
                              momentum_schedule_linear_const=opt.get('schedule_linear', 1.0),  # scale the momentum step-size
                              momentum_schedule_linear_period=opt.get('schedule_linear_period', 10)
                              )
@@ -456,22 +460,37 @@ class SRCutils(Optimizer):
         H_bmm = lambda x: self.hessian_vector_product(x)
         return self.get_eigen(H_bmm, **kwargs)
 
-    def get_grads_and_params(self):
+    def get_grads_and_params(self, grad1=False):
         """ Get model parameters and corresponding gradients
         """
         params = []
         grads = []
         # We assume only one group for now
+        #print(self.param_groups)
         for param in self.param_groups[0]['params']:
             if (not (param.grad is None) and not (param.grad.sum() == 0)) \
                     or self.defaults['problem'] != 'matrix_completion':
                 params.append(param)
-            if param.grad is None or \
-                    (param.grad.sum() == 0
-                     and self.defaults['problem'] == 'matrix_completion'):
+            if (param.grad is None or \
+                    (param.grad.sum() == 0)) \
+                     and self.defaults['problem'] == 'matrix_completion':
                 continue
 
+            #print('each param', param.size(), param)
+            #print('each grad', param.grad1)
+            #print('its mean', param.grad, param.grad1.mean(dim=0))
+            assert (torch.allclose(param.grad1.mean(dim=0), param.grad, atol=1e-05))
+
+
             grads.append(param.grad + 0.)
+
+        #print(len(flatten_tensor_list(grads)),
+        #      flatten_tensor_list(grads))
+        #exit(0)
+
+        #if grad1:
+        #    return flatten_tensor_list(grads), params,
+        #else:
         return flatten_tensor_list(grads), params
 
     def update_params(self, delta, inplace=True):
@@ -704,6 +723,18 @@ class SRCutils(Optimizer):
                     #self.grad = self.grad.to(torch.float64)
                     print(exitCode, update.norm(p=2), update)
                     #exit(0)
+                elif self.defaults['subproblem_solver'] == 'AdaHess':
+                    ihvp = self.hessian_vector_product(-g_, inverse=True,
+                                                       shift=((self.defaults['sigma'] / 2) * delta.norm(p=2)).detach().numpy())
+                    delta_new = ihvp
+                    update = delta - delta_new
+                elif self.defaults['subproblem_solver'] == 'AdaHess_newton':
+                    update = self.hessian_vector_product(self.m_grad(g_, delta), inverse=True,
+                                                         shift=((self.defaults['sigma']) * delta.norm(p=2)).detach().numpy())
+                    self.computations_done += 1
+                    self.computations_done_times_samples += \
+                        self.defaults['sample_size_hessian']
+
                 update_norm = update.norm(p=2)
                 # ToDo - improve / remove empirical checks
                 if False and (not self.defaults['AdaHess']) and (update_norm >= 2.5 * self.mean_update) and self.mean_update != 0:
@@ -983,7 +1014,7 @@ class SRCutils(Optimizer):
             .detach().cpu().numpy()
         return output
 
-    def hessian_vector_product(self, v):
+    def hessian_vector_product(self, v, inverse=False, shift=None):
         """
         compute the hessian vector product of Hv, where
         gradsH is the gradient at the current point,
@@ -1010,7 +1041,7 @@ class SRCutils(Optimizer):
             data = data.to(self.defaults['dev'])
             target = target.to(self.defaults['dev'])
 
-        # if self.first_hv:
+        #if self.first_hv:
         self.zero_grad()
         x = self.param_groups[0]['params']
 
@@ -1018,15 +1049,22 @@ class SRCutils(Optimizer):
             data_ = index_to_params(data, x)
             self.loss_fn(self.model(data_), target, data_[0], data_[1]).backward(create_graph=True)
         elif self.is_classification or self.is_AE:  # and self.first_hv:
+
             outputs = self.model(data)
             self.y_onehot_hess.zero_()
             if 'LIN_REG' in self.defaults['problem']:
                 self.y_onehot_hess.scatter_(1, target.view(-1, 1), 1)
             #print(outputs, self.y_onehot)
+            try:
+                autograd_hacks.clear_backprops(self.model)
+            except:
+                pass
             self.loss_fn(outputs, self.y_onehot_hess if 'LIN_REG' in self.defaults['problem'] else target)\
                 .backward(create_graph=True)
+            autograd_hacks.compute_grad1(self.model)
+
             #self.loss_fn(outputs, target).backward(create_graph=True)
-            # self.first_hv = False
+            #self.first_hv = False
         elif self.is_w_function:  # and self.first_hv:
             self.model(x[0]).backward(create_graph=True)
             self.perturb()
@@ -1050,6 +1088,7 @@ class SRCutils(Optimizer):
                 v_temp = torch.cat((v, torch.zeros(len(gradsh) - len(v))))
 
             if self.defaults['AdaHess']:
+                #if self.first_hv:
                 self.t_hess += 1
                 b = torch.distributions.bernoulli.Bernoulli(torch.tensor([0.5]))
                 rad = (2 * b.sample(self.grad.size()) - 1).squeeze()
@@ -1058,30 +1097,41 @@ class SRCutils(Optimizer):
                 print('D', self.t_hess, D.size(), D)
                 self.D = (self.b_2 * self.D + (1 - self.b_2) * D ** 2).detach()
                 hv = torch.sqrt((self.D / (1 - self.b_2 ** self.t)))
+                self.hv_temp = hv.detach()
+                #self.first_hv = False
+                #else:
+                #    pass
             else:
                 hv = torch.autograd.grad(gradsh, data_, grad_outputs=v_temp,
                                      only_inputs=True, retain_graph=True)
 
         else:
             if self.defaults['AdaHess']:
+
+                #if self.first_hv:
                 print('Start AdaHess for hvp')
                 self.t_hess += 1
                 # Temporal & spatial averaging
-                b = torch.distributions.bernoulli.Bernoulli(torch.tensor([0.5]))
-                rad = (2 * b.sample(self.grad.size()) - 1).squeeze()
+                ber = torch.distributions.bernoulli.Bernoulli(torch.tensor([0.5]))
+                rad = (2 * ber.sample(self.grad.size()) - 1).squeeze()
                 D = torch.autograd.grad(gradsh, params, grad_outputs=rad,
                                      only_inputs=True, retain_graph=True)
                 D = (rad * flatten_tensor_list(D))
                 n_ = len(D)
 
-                # Before the code-block was here
                 print('D', D.norm(p=2))
                 self.D = (self.b_2 * self.D + (1 - self.b_2) * D ** 2).detach()
                 print('D', D.norm(p=2))
                 hv = torch.sqrt((self.D / (1 - self.b_2 ** self.t_hess)))
+                self.hv_temp = hv.detach()
+                #self.first_hv = False
+                hv = self.hv_temp
 
-                # Now the code-block is here (D -> hv)
+                if inverse:
+                    hv = (hv + shift) ** -1
+
                 b = self.defaults['block_size']
+
                 if b is not None:
                     hv = torch.cat((hv[:b * (n_ // b)].reshape(-1, b)
                                    .mean(1).repeat_interleave(b), hv[b * (n_ // b):])) \