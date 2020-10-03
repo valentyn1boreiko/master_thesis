434a6cce4bdece945620a25e824e2a9676a448f6
valentyn.boreiko@ibm.com <valentyn.boreiko@ibm.com>
2020-10-03 16:40:26 +0200

    grad1 shape - fix

---
+++ code/docker_experiments/plot.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import glob
+
+paths = [x + '_src_final' for x in ['resnet', 'ae', 'linear_reg', 'cnn']]
+criterion = 'train_losses'
+
+for path in paths:
+    files = glob.glob(path + "/*.csv")
+    cts = 'computations_times_sample'
+
+    dfs = {}
+
+    for file in files:
+        if 'H_size=100' not in file and 'LBFGS_Solver=Newton' not in file:
+            split_ = file.split('/')[-1].split("_")
+            label = "{}".format(split_[3]) \
+                if 'loss' not in file else \
+                'SRC, ' + split_[5]+'_' + split_[6] + ', ' + split_[7].replace('Linear', 'Linear_system')
+            dfs[label] = pd.read_csv(file)
+
+    for label, df in dfs.items():
+        plt.plot(df[cts if cts in df.columns else 'samples'][:], df[criterion][:],
+                 label=label)
+
+    plt.legend(bbox_to_anchor=(1.04, 1), loc='upper left')
+    plt.title(path)
+    plt.xlabel('Oracle calls')
+    plt.ylabel('Training Loss')
+    plt.savefig(path + '/final.png', bbox_inches="tight")
+    plt.clf()
---
+++ code/docker_experiments/search.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from pathlib import Path
+import numpy as np
+from shutil import copy
+import pprint
+
+path = 'resnet_src'
+criterion = 'train_losses'
+
+terms = ['AdaHess_Solver=adaptive', 'AdaHess_Solver=Linear_system',
+         'AdaHess_Solver=Newton', 'AdaHess_Solver=non-adaptive',
+         'LBFGS_Solver=Newton', 'None_Solver=non-adaptive',
+         'WoodFisher_Solver=Newton', 'LBFGS_Solver=Linear_system', 'None_Solver=adaptive',
+         'WoodFisher_Solver=Linear_system']
+term_dicts = {key: {'loss': np.inf, 'path': ''} for key in terms}
+
+for path_ in Path(path).rglob('*.csv'):
+    for t in terms:
+        if t in path_.name:
+            if term_dicts[t]['loss'] > pd.read_csv(path_)[criterion].iloc[-1]:
+                term_dicts[t]['path'] = path_
+                term_dicts[t]['loss'] = pd.read_csv(path_)[criterion].iloc[-1]
+
+pprint.pprint(term_dicts)
+for (el, key) in term_dicts.items():
+    #print(key['path'])
+    try:
+        copy(src=key['path'], dst=path + '_final/', follow_symlinks=True)
+    except:
+        print(key['path'])
